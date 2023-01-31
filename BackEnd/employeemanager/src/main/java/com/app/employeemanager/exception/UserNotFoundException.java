e3e03d5f0536146900b6178780d9af54a74deda6
ninadkadam123 <ninadkadam123@gmail.com>
2023-01-31 11:31:15 +0530

    first commit

---
+++ BackEnd/employeemanager/src/main/java/com/app/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.app.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+	
+	public UserNotFoundException(String message) {
+		
+		super(message);
+		
+	}
+
+}
